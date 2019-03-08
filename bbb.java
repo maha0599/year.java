b1aa9dd26b49f54b2e24193b092cbdfb5a742011
maha0599 <47376939+maha0599@users.noreply.github.com>
2019-03-08 14:22:10 +0530

    Create bbb.java

---
+++ bbb.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,n;
+		Scanner s = new Scanner(System.in);
+    n=s.nextInt();
+    for(i=0;i<n;i++)
+    {
+    System.out.println("Hello");
+    }
+}
+}
+	
