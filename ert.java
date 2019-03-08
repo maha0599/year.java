d5fc18214eeac975b46bca50d245e8d1d378f1fd
maha0599 <47376939+maha0599@users.noreply.github.com>
2019-03-08 14:15:34 +0530

    Create ert.java

---
+++ ert.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			int n,sum=0,k;
+			Scanner s=new Scanner(System.in);
+			n=s.nextInt();
+			k=s.nextInt();
+	        int array[]=new int[n];
+            for(int i=0;i<n;i++)
+            {
+            	array[i]=s.nextInt();
+            }
+              for(int i=0;i<k;i++)
+	         {
+             sum=sum+array[i];
+        	}
+	       System.out.println(sum);
+	}
+}
